a484eecc6fd1c4635ccfdce2672c09eebf0b5552
Mellisa Damayanti <114962318+mellisadmyn@users.noreply.github.com>
2022-10-30 12:42:03 +0800

    Add files via upload

---
+++ 2. Greenfoot/Oracle Academy/JF 3-01 Getting Started with Greenfoot/Project/JF_V02_S03_L01PrjStudent/RobotWorld.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @mellisadmyn (your name) 
+ * @version (a version number or a date)
+ */
+public class RobotWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public RobotWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Robot robot = new Robot();
+        addObject(robot,280,203);
+    }
+}
